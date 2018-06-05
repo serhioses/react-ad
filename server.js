1da9073b480947caaa40a74404e715dedef5e2ff
serhioses <serega.mirosh@gmail.com>
2018-06-05 17:48:13 +0300

    redirect

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const PORT = 8000;
 
 app.use(express.static('app'));
 
+app.get('/*', (request, response) => {
+  response.redirect('/');
+});
+
 app.listen(PORT, () => {
   console.log(`Express is listening on port ${PORT}`);
 });
