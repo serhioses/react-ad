e206d1a6f15035cbd19e943273a18437d644e90f
serhioses <serega.mirosh@gmail.com>
2018-06-05 23:04:11 +0300

    server.js https

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const PORT = 8000;
 
 app.use(express.static('app'));
 
+app.use(function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Express is listening on port ${PORT}`);
 });
