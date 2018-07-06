e09b342f061f191af59e0f190f0a048656cd1efc
serhioses <serega.mirosh@gmail.com>
2018-07-06 10:23:12 +0300

    env file

--- app/dev/js/firebase/firebase.js
+++ app/dev/js/firebase/firebase.js
@@ -4,12 +4,18 @@ import 'firebase/database';
 import 'firebase/storage';
 
 const config = {
-  apiKey: "AIzaSyAhbC5Cmmrrz5WXPfD7KW17R7JH54Wu0C0",
-  authDomain: "react-ad.firebaseapp.com",
-  databaseURL: "https://react-ad.firebaseio.com",
-  projectId: "react-ad",
-  storageBucket: "react-ad.appspot.com",
-  messagingSenderId: "26139567369"
+  // apiKey: "AIzaSyAhbC5Cmmrrz5WXPfD7KW17R7JH54Wu0C0",
+  // authDomain: "react-ad.firebaseapp.com",
+  // databaseURL: "https://react-ad.firebaseio.com",
+  // projectId: "react-ad",
+  // storageBucket: "react-ad.appspot.com",
+  // messagingSenderId: "26139567369"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
 };
 
 if (!firebase.apps.length) {
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,43 @@
 const webpack = require('webpack');
+const envFile = require('node-env-file');
 const path = require('path');
 const jsDir = path.resolve(__dirname, 'app/dev/js');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 const isDevelopment = process.env.NODE_ENV === 'development';
+const isProduction = process.env.NODE_ENV === 'production';
+
+try {
+  envFile(path.join(__dirname, 'config/' + process.env.NODE_ENV.trim() + '.env'));
+} catch (err) {
+  console.log(err);
+}
+
+const plugins = [
+  new webpack.NoEmitOnErrorsPlugin(),
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      API_KEY: JSON.stringify(process.env.API_KEY),
+      AUTH_DOMAIN: JSON.stringify(process.env.AUTH_DOMAIN),
+      DATABASE_URL: JSON.stringify(process.env.DATABASE_URL),
+      PROJECT_ID: JSON.stringify(process.env.PROJECT_ID),
+      STORAGE_BUCKET: JSON.stringify(process.env.STORAGE_BUCKET),
+      MESSAGING_SENDER_ID: JSON.stringify(process.env.MESSAGING_SENDER_ID),
+    }
+  })
+];
+
+if (isProduction) {
+  plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false,
+      }
+    }),
+  );
+}
 
 module.exports = {
   context: path.resolve(__dirname, 'app'),
@@ -40,9 +74,7 @@ module.exports = {
       }
     ]
   },
-  plugins: [
-    new webpack.NoEmitOnErrorsPlugin()
-  ],
+  plugins: plugins,
   devtool: isDevelopment ? 'cheap-inline-module-source-map' : false,
   watch: isDevelopment,
   mode: process.env.NODE_ENV
