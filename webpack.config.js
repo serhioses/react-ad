5d88eae7316ea2e9c77a8b06b38273a8a7475fe6
serhioses <serega.mirosh@gmail.com>
2018-07-06 11:41:45 +0300

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack');
 const envFile = require('node-env-file');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const path = require('path');
 const jsDir = path.resolve(__dirname, 'app/dev/js');
 
@@ -11,31 +12,23 @@ const isProduction = process.env.NODE_ENV === 'production';
 try {
   envFile(path.join(__dirname, 'config/' + process.env.NODE_ENV.trim() + '.env'));
 } catch (err) {
-  console.log(err);
+  // console.log(err);
 }
 
-const plugins = [
-  new webpack.NoEmitOnErrorsPlugin(),
-  new webpack.DefinePlugin({
-    'process.env': {
-      NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-      API_KEY: JSON.stringify(process.env.API_KEY),
-      AUTH_DOMAIN: JSON.stringify(process.env.AUTH_DOMAIN),
-      DATABASE_URL: JSON.stringify(process.env.DATABASE_URL),
-      PROJECT_ID: JSON.stringify(process.env.PROJECT_ID),
-      STORAGE_BUCKET: JSON.stringify(process.env.STORAGE_BUCKET),
-      MESSAGING_SENDER_ID: JSON.stringify(process.env.MESSAGING_SENDER_ID),
-    }
-  })
-];
+const minimizer = [];
 
 if (isProduction) {
-  plugins.push(
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        warnings: false,
-      }
-    }),
+  minimizer.push(
+    new UglifyJsPlugin({
+      cache: true,
+      parallel: true,
+      uglifyOptions: {
+        compress: false,
+        ecma: 6,
+        mangle: true,
+      },
+      sourceMap: false
+    })
   );
 }
 
@@ -74,7 +67,23 @@ module.exports = {
       }
     ]
   },
-  plugins: plugins,
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+        API_KEY: JSON.stringify(process.env.API_KEY),
+        AUTH_DOMAIN: JSON.stringify(process.env.AUTH_DOMAIN),
+        DATABASE_URL: JSON.stringify(process.env.DATABASE_URL),
+        PROJECT_ID: JSON.stringify(process.env.PROJECT_ID),
+        STORAGE_BUCKET: JSON.stringify(process.env.STORAGE_BUCKET),
+        MESSAGING_SENDER_ID: JSON.stringify(process.env.MESSAGING_SENDER_ID),
+      }
+    })
+  ],
+  optimization: {
+    minimizer: minimizer,
+  },
   devtool: isDevelopment ? 'cheap-inline-module-source-map' : false,
   watch: isDevelopment,
   mode: process.env.NODE_ENV
